405c8d4b33da717067b502519b732272bd37943c
AdonisCodes <122154257+AdonisCodes@users.noreply.github.com>
2023-08-06 05:27:53 +0000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+## How to setup the server...
+
+1. I have sent you a email to make you admin of the database ( It will make you the owner and I will leave which means no setup on your side)
+2. I have setup the server code to use Render.com ( A free API hosting platform ) So no setup for that on your side as well
+
+### Urls
+Database -> Sent you an url to manage the Database as an Admin ( Will leave once both of us knows the program works )
+API -> 
+
+### Limitations
+1. The databas will be able to host around 300k to 500k emails ( 20mb of free space )
+
+### I will be transfering ownership for everything to you.
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Create a PostgreSQL pool
+const pool = new Pool({
+  user: 'wfahmwhm', // The username provided for the database
+  host: 'bubble.db.elephantsql.com', // The host the server is running on ( Usually called host / Server and is an url similar to this name.db.name.com)
+  database: 'wfahmwhm', // The database being used
+  password: 'Mr73vlDpngbtfF9FJwdyPQvGPdzHhv8k', // The password for the user / database
+  port: 5432, // Default PostgreSQL port
+});
+
+// Middleware
+app.use(bodyParser.json());
+
+// Route to handle POST requests to /submit
+app.post('/submit', async (req, res) => {
+  try {
+    const { userID, email } = req.body;
+
+    // Validate the user email and ID
+    let re = /\S+@\S+\.\S+/;
+    if (!userID || !email || !re.test(email) || userID.length < 32) {
+      return res.status(403).json({ error: 'Invalid userID or email' });
+    }
+
+    // Insert the email to the database
+    const query = 'INSERT INTO email_list (userID, email) VALUES ($1, $2)';
+    const values = [userID, email];
+
+    await pool.query(query, values);
+
+    return res.status(200).json({ status: 200, message: 'Data submitted successfully'});
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({"status": 500, error: 'Internal Server Error with error message: \n ' + error });
+  }
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
